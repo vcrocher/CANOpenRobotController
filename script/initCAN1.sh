0720e615c3463b5bd2d01d4135e407762f6ee16c
vcrocher <16175577+vcrocher@users.noreply.github.com>
2022-08-24 16:13:59 +1000

    Restored pin-config in initCAN1 script: needed for BBB

--- script/initCAN1.sh
+++ script/initCAN1.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-
+echo "Configure CAN1 pins"
+sudo config-pin p9.24 can
+sudo config-pin p9.26 can
 echo "Enabling CAN1"
 sudo ip link set can1 up type can bitrate 1000000
 sudo ifconfig can1 up
